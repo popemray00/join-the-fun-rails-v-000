3a5c13b5791e096a873fee5ee60ac9dd7630a976
Michael Pope <popemray1986@gmail.com>
2018-07-21 16:44:27 +0000

    automatically backed up by learn

--- db/migrate/20180721164259_jointable.rb
+++ db/migrate/20180721164259_jointable.rb
@@ -1,4 +1,9 @@
 class Jointable < ActiveRecord::Migration
   def change
+    create_table :join do |t|
+      t.integer :passenger_id
+      t.integer :taxi_id
+
+      t.timestamps null: false
   end
 end
