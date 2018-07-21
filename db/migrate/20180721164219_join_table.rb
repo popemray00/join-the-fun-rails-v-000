98abd4e10dfe10fe603a03d00e792c505eaed746
Michael Pope <popemray1986@gmail.com>
2018-07-21 16:42:33 +0000

    automatically backed up by learn

--- db/migrate/20180721164219_join_table.rb
+++
@@ -1,8 +0,0 @@
-class JoinTable < ActiveRecord::Migration
-  def change
-    create_join_table :passengers, :taxis do |t|
-      # t.index [:passenger_id, :taxi_id]
-      # t.index [:taxi_id, :passenger_id]
-    end
-  end
-end
